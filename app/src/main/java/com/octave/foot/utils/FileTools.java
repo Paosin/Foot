756828b332bafb01cd7335bf45b84e911b9de65b
HS <71546563@qq.com>
2017-01-10 11:17:56 +0800

    我TM也不知道写了些啥鬼

--- app/src/main/java/com/octave/foot/utils/FileTools.java
+++ app/src/main/java/com/octave/foot/utils/FileTools.java
@@ -1,9 +1,58 @@
 package com.octave.foot.utils;
 
+import android.os.Environment;
+import android.util.Log;
+
+import com.octave.foot.bean.CenterOfPressure;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 /**
+ * 使用jxl来导入Excel
  * Created by Paosin Von Scarlet on 2017/1/9.
  */
 
 public class FileTools {
+    private String sdPath(String path) {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            File sdFile = Environment.getExternalStorageDirectory();
+            String filePath = sdFile.getAbsolutePath();
+            return filePath + "/" + path;
+        }
+        return null;
+    }
 
+    public List<CenterOfPressure> readExcel(String path) {
+        List<CenterOfPressure> list = new ArrayList<CenterOfPressure>();
+        try {
+            Workbook workbook = Workbook.getWorkbook(new File(sdPath(path)));
+            //获取第一个工作表的对象
+            Sheet sheet = workbook.getSheet(0);
+            //获取第一列第一行的的单元格
+            for (int i = 0; i < sheet.getRows(); i++) {
+                CenterOfPressure centerOfPressure = new CenterOfPressure();
+                centerOfPressure.setFrame(Integer.parseInt(sheet.getCell(0, i).getContents()));
+                centerOfPressure.setMs(Float.parseFloat(sheet.getCell(1, i).getContents()));
+                centerOfPressure.setX(Float.parseFloat(sheet.getCell(2, i).getContents()));
+                centerOfPressure.setY(Float.parseFloat(sheet.getCell(3, i).getContents()));
+                centerOfPressure.setForce(Integer.parseInt(sheet.getCell(4, i).getContents()));
+                list.add(centerOfPressure);
+            }
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
--- app/src/main/java/com/octave/foot/view/DrawPressurePath.java
+++ app/src/main/java/com/octave/foot/view/DrawPressurePath.java
@@ -6,8 +6,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+
+import com.octave.foot.bean.CenterOfPressure;
+import com.octave.foot.utils.FileTools;
+
+import java.util.List;
 
 /**
  * Created by Paosin Von Scarlet on 2017/1/9.
@@ -17,83 +23,59 @@ public class DrawPressurePath extends View {
 
     private float mX;
     private float mY;
-
+    private float mWidth;
+    private float mHeight;
+    private int count = 0;
     private final Paint mGesturePaint = new Paint();
     private final Path mPath = new Path();
+    private FileTools file = new FileTools();
+    private DisplayMetrics dm = new DisplayMetrics();
+    List<CenterOfPressure> data;
 
-    public DrawPressurePath(Context context)
-    {
+    public DrawPressurePath(Context context) {
         super(context);
         mGesturePaint.setAntiAlias(true);
         mGesturePaint.setStyle(Paint.Style.STROKE);
         mGesturePaint.setStrokeWidth(5);
         mGesturePaint.setColor(Color.BLACK);
+        dm = context.getResources().getDisplayMetrics();
+        mWidth = dm.widthPixels;
+        mHeight = dm.heightPixels;
+        data = file.readExcel("left_foot.xls");
+        mX = data.get(0).getX() + mWidth / 2;
+        mY = mHeight / 2 - data.get(0).getY();
+        mPath.moveTo(mX, mY);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        // TODO Auto-generated method stub
-        switch (event.getAction())
-        {
-            case MotionEvent.ACTION_DOWN:
-                touchDown(event);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                touchMove(event);
-        }
-        //更新绘制
-        invalidate();
-        return true;
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas)
-    {
+    protected void onDraw(Canvas canvas) {
         // TODO Auto-generated method stub
         super.onDraw(canvas);
-        //通过画布绘制多点形成的图形
-        canvas.drawPath(mPath, mGesturePaint);
+        for (int i = 1; i < data.size(); i++) {
+            pointRead(data.get(i));
+            //通过画布绘制多点形成的图形
+            canvas.drawPath(mPath, mGesturePaint);
+        }
     }
 
-    //手指点下屏幕时调用
-    private void touchDown(MotionEvent event)
-    {
-
-        //mPath.rewind();
-        //重置绘制路线，即隐藏之前绘制的轨迹
-        mPath.reset();
-        float x = event.getX();
-        float y = event.getY();
-
-        mX = x;
-        mY = y;
-        //mPath绘制的绘制起点
-        mPath.moveTo(x, y);
-    }
-
-    //手指在屏幕上滑动时调用
-    private void touchMove(MotionEvent event)
-    {
-        final float x = event.getX();
-        final float y = event.getY();
-
+    private void pointRead(CenterOfPressure cop) {
+        final float x = mWidth / 2 + cop.getX();
+        final float y = mHeight / 2 - cop.getY();
+        System.out.println(count + ":" + x + "=" + mWidth / 2 + "+" + cop.getX() + "-------" + y + "=" + mHeight / 2 + "-" + cop.getY());
+        count++;
         final float previousX = mX;
         final float previousY = mY;
 
         final float dx = Math.abs(x - previousX);
         final float dy = Math.abs(y - previousY);
-
         //两点之间的距离大于等于3时，生成贝塞尔绘制曲线
-        if (dx >= 3 || dy >= 3)
-        {
+        if (dx >= 3 || dy >= 3) {
             //设置贝塞尔曲线的操作点为起点和终点的一半
             float cX = (x + previousX) / 2;
             float cY = (y + previousY) / 2;
 
             //二次贝塞尔，实现平滑曲线；previousX, previousY为操作点，cX, cY为终点
             mPath.quadTo(previousX, previousY, cX, cY);
-
             //第二次执行时，第一次结束调用的坐标值将作为第二次调用的初始坐标值
             mX = x;
             mY = y;
